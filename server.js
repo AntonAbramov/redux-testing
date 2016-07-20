197855095a6f92017c095326f7263258dc4b20e0
antonabramov <anton@juniqe.com>
2016-07-20 09:05:33 +0200

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 3300));
+app.set('port', (process.env.PORT || 4000));
 app.use('/', express.static(__dirname + '/app/assets/'));
 
 app.get('/cart', function(req, res){
@@ -20,19 +20,6 @@ app.get('/cart', function(req, res){
       'size': 'Size : 40x60',
       'avaliableText': 'Available: delivery in 4 to 7 working days',
       'avaliable': true,
-      'selected' : '3',
-      'select': [
-        {'value' : '1', 'name' : '1'},
-        {'value' : '2', 'name' : '2'},
-        {'value' : '3', 'name' : '3'},
-        {'value' : '4', 'name' : '4'},
-        {'value' : '5', 'name' : '5'},
-        {'value' : '6', 'name' : '6'},
-        {'value' : '7', 'name' : '7'},
-        {'value' : '8', 'name' : '8'},
-        {'value' : '9', 'name' : '9'},
-        {'value' : '10', 'name' : '10'}
-      ],
       'selectData' : {
         'quantity' : 2,
         'data' : [
